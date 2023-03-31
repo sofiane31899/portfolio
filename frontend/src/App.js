5d84db7a52bd46fb54f483c4a74d7eed8530a9dc
Bacha rabah <bacharabah31899@gmail.com>
2023-03-31 17:07:34 +0100

    starting the animating with svg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-800 flex flex-col justify-center items-center min-h-[400px] p-[100px]">
+      <div className="bg-red-500 relative w-full h-[225px] text-center">
+         lorem ipsum fhsju sjhiz sdjoisufo jishfoqknf uhfioqzekfnui jshfoiQNFUGH SFJqosfknqiofqs 
+      <div className="absolute w-full h-[225px] bottom-0 rounded-full "/> 
+      </div>
+      <div className="bg-blue-500 w-full h-[225px] text-center"> lorem ipsum fhsju sjhiz sdjoisufo jishfoqknf uhfioqzekfnui jshfoiQNFUGH SFJqosfknqiofqs  </div>
+      <div className="bg-orange-500 w-full h-[225px] text-center"> lorem ipsum fhsju sjhiz sdjoisufo jishfoqknf uhfioqzekfnui jshfoiQNFUGH SFJqosfknqiofqs  </div>
+      <div className="bg-purple-500 w-full h-[225px] text-center"> lorem ipsum fhsju sjhiz sdjoisufo jishfoqknf uhfioqzekfnui jshfoiQNFUGH SFJqosfknqiofqs  </div>
+
+
+
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+   "./src/**/*.{jsx,ts,tsx,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
