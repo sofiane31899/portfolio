c783092fc70617fc3761eed655d66af106053de7
Bacha rabah <bacharabah31899@gmail.com>
2023-07-08 19:53:36 +0100

    better designe

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -5,9 +5,10 @@ import Project from "./Project";
 import frontendapp from "./../assets/frontendapp.PNG";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import netflix from "./../assets/projects/netflix.PNG";
 
 function Projects() {
-  const [currentProject, setCurrentProject] = useState(1);
+  const [currentProject, setCurrentProject] = useState(0);
   const projects = [
     {
       titre: "Krilli APP ",
@@ -21,11 +22,20 @@ function Projects() {
     {
       titre: "frontend",
       description:
-        " this is a web application that I built using React, Framer Motion, and Tailwind CSS. The app is designed to provide an engaging and interactive user experience, with fluid animations and transitions that bring the interface to life.",
+        " this is a web application that I built using React, and Tailwind CSS. The app is designed to provide an engaging and interactive user experience, with fluid animations and transitions that bring the interface to life.",
       image: frontendapp,
-      technologies: ["react", "tailwind", "framer motion"],
+      technologies: ["react", "tailwind"],
       codeURL: "https://github.com/sofiane31899/frontend-application",
       live: "https://sofiane31899.github.io/frontend-application/",
+    },
+    {
+      titre: "Netflix clone ",
+      description:
+        "This project involves creating a web application that closely resembles Netflix, using a technology stack consisting of Node.js, React, and MongoDB. The goal is to replicate and offer users similar functionalities and features found on the popular streaming platform. By leveraging Node.js for the backend, React for the frontend, and MongoDB as the database.",
+      image: netflix,
+      technologies: ["react", "mongodb", "express"],
+      codeURL: "https://github.com/sofiane31899/netflix",
+      live: " https://649ef60a50d4bf0b6f7df726--subtle-bublanina-41761e.netlify.app/",
     },
   ];
 
@@ -38,7 +48,7 @@ function Projects() {
   };
 
   return (
-    <div className="h-screen flex flex-col   pt-[80px] relative space-y-1 ">
+    <div className="h-screen flex flex-col group  pt-[80px] relative space-y-1 ">
       <h1 className="text-blue-500 font-bold text-2xl  px-10 mb-5 md:pl-[100px]">
         projects
       </h1>
@@ -51,15 +61,19 @@ function Projects() {
       </div>
       <div
         onClick={() => rightClick()}
-        className="absolute top-[50%] md:right-[10%] right-3 cursor-pointer rounded-full bg-gray-600 text-white"
+        className={`${
+          currentProject == 2 ? "hidden" : "group-hover:block"
+        } absolute top-[50%] md:right-[10%] hidden right-3 cursor-pointer rounded-full text-2xl  text-gray-800`}
       >
-        <ChevronRightIcon />
+        <ChevronRightIcon sx={{ fontSize: 40 }} />
       </div>
       <div
         onClick={() => leftClick()}
-        className="absolute top-[50%] md:left-[10%] left-3 cursor-pointer rounded-full bg-gray-600 text-white"
+        className={`${
+          currentProject == 0 ? "hidden" : "group-hover:block"
+        } absolute top-[50%] md:left-[10%] hidden  left-3 cursor-pointer rounded-full text-2xl  text-gray-800`}
       >
-        <ChevronLeftIcon />
+        <ChevronLeftIcon sx={{ fontSize: 40 }} />
       </div>
     </div>
   );
